57f80dc91059a07d25f0db4b1cce31b2b47bace7
Ben Horne <benhorne44@gmail.com>
2014-01-15 21:19:11 -0700

    shortens ternary lines by adding insert methods for node class

--- binary_search/bst.rb
+++ binary_search/bst.rb
@@ -20,7 +20,7 @@ class Bst
 
   def add_left(node, data)
     if data <= node.data
-      node.left.nil? ? (node.left = Node.new(data)) : add_left(node.left, data)
+      node.left.nil? ? node.add_left(data) : add_left(node.left, data)
     else
       add_right(node, data)
     end
@@ -28,7 +28,7 @@ class Bst
 
   def add_right(node, data)
     if data > node.data
-      node.right.nil? ? (node.right = Node.new(data)) : add_right(node.right, data)
+      node.right.nil? ? node.add_right(data) : add_right(node.right, data)
     else
       add_left(node, data)
     end
@@ -53,6 +53,14 @@ class Node
     @data = content
     @left = nil
     @right = nil
+  end
+
+  def add_left(data)
+    @left = Node.new(data)
+  end
+
+  def add_right(data)
+    @right = Node.new(data)
   end
 
 end
