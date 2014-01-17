b8afb98f4b89f68a7a798da87223530a8a824913
Ben Horne <benhorne44@gmail.com>
2014-01-17 12:22:37 -0700

    adds simple implementation for testing count and pushing to the list, leading to creating a node class

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,3 +1,11 @@
 class LinkedList
 
+  def count
+    @data ? 1 : 0
+  end
+
+  def push(data)
+    @data = data
+  end
+
 end
--- linked_list/linked_list_spec.rb
+++ linked_list/linked_list_spec.rb
@@ -4,4 +4,16 @@ describe LinkedList do
   it "exists" do
     expect(LinkedList).to be
   end
+
+  it "has a default count of zero" do
+    list = LinkedList.new
+    expect(list.count).to eq(0)
+  end
+
+  it "can add an object to the list" do
+    list = LinkedList.new
+    expect(list.count).to eq(0)
+    list.push('howdy')
+    expect(list.count).to eq(1)
+  end
 end
